e3777c11e37614dacff7fcbfde5f2907035376a7
Charles Yarnold <charlesyarnold@gmail.com>
2017-01-02 23:01:00 +0000

    Add config file

---
+++ Inc/config.h
@@ -0,0 +1,24 @@
+/**
+  ******************************************************************************
+  * File Name          : config.h
+  * Description        : Individual device config
+  ******************************************************************************
+  *
+  * Copyright (c) 2017 Charles Yarnold
+  * All rights reserved.
+  *
+  ******************************************************************************
+  */
+
+#ifndef __CONFIG_H__
+#define __CONFIG_H__
+
+#define HOSTNAME "dometroller-1"
+#define MACADDR0 0xde
+#define MACADDR1 0xad
+#define MACADDR2 0xbe
+#define MACADDR3 0xef
+#define MACADDR4 0xfe
+#define MACADDR5 0xed
+
+#endif
